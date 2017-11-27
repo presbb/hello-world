902f130b5ef2df6d772dd411d42cf22c055e5ea4
Geoff Whatley <me@geoffwhatley.com>
2017-11-27 11:36:32 +1100

    feat(intro): personalize welcome message with user input
    reimplemented welcome message to fit outline in the initial README's contents, and allow for user
    input to allow personalization. hopefully this increases immersion. refactored the README to fit a
    more standard format, and clarified purpose. lifted build instructions from code to README.

--- hello.c
+++ hello.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 
-// Compile and run the code with: 
-// gcc hello.c -o hello
-// ./hello or ./hello.exe
-
 int main() {
-    printf("Hello World!");
-    return 0;
+  char name[30];
+
+  printf("Welcome to Varrock. What's your name, adventurer?\n");
+
+  scanf("%s", &name);
+
+  printf("Beware of the Dark Wizards, %s.\n", name);
+
+  // TODO: more gameplay
+
+  return 0;
+
 }
